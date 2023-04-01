9a5332d98340f0c87b1fa795c1b5fa4ea4a68a9b
Your Name <you@example.com>
2023-04-01 20:45:23 +0800

    none

--- configs/config.py
+++ configs/config.py
@@ -20,13 +20,20 @@ NICKNAME: str = "小真寻"
 # 数据库（必要）
 # 如果填写了bind就不需要再填写后面的字段了#）
 # 示例："bind": "postgres://user:password@127.0.0.1:5432/database"
+#bind: str = ""  # 数据库连接链接
+#sql_name: str = "postgres"
+#user: str = ""  # 数据用户名
+#password: str = ""  # 数据库密码
+#address: str = ""  # 数据库地址
+#port: str = ""  # 数据库端口
+#database: str = ""  # 数据库名称
 bind: str = ""  # 数据库连接链接
 sql_name: str = "postgres"
-user: str = ""  # 数据用户名
-password: str = ""  # 数据库密码
-address: str = ""  # 数据库地址
-port: str = ""  # 数据库端口
-database: str = ""  # 数据库名称
+user: str = "uname"
+password: str = "zhenxun"
+address: str = "127.0.0.1"
+port: str = "5432"
+database: str = "testdb"
 
 # 代理，例如 "http://127.0.0.1:7890"
 # 如果是WLS 可以 f"http://{hostip}:7890" 使用寄主机的代理
